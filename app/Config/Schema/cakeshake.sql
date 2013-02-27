b4dd1041fda6ab3d98733c8fe4b0ced2e2ff21f3
Fournier Trecy <trecy.lefort@gmail.com>
2013-02-27 09:47:00 +0100

    modification de la base de donnée
    j'ai changer le login et le mot de passe à la place de root j'ai mis
    cakephp et mot de passe cakephp

--- app/Config/Schema/cakeshake.sql
+++ app/Config/Schema/cakeshake.sql
@@ -1,120 +1,137 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Client: localhost
--- Généré le : Mar 26 Février 2013 à 10:16
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4004
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Hôte: localhost (MySQL 5.5.29)
+# Base de données: cakeshake
+# Temps de génération: 2013-02-27 08:43:20 +0000
+# ************************************************************
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données: `cakeshake`
---
 
--- --------------------------------------------------------
+# Affichage de la table chats
+# ------------------------------------------------------------
 
---
--- Structure de la table `chats`
---
+DROP TABLE IF EXISTS `chats`;
 
-CREATE TABLE IF NOT EXISTS `chats` (
+CREATE TABLE `chats` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_from` int(11) NOT NULL,
   `user_to` int(11) NOT NULL,
   `message` text NOT NULL,
   `sent` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `chats`
---
+LOCK TABLES `chats` WRITE;
+/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
 
-INSERT INTO `chats` (`id`, `user_from`, `user_to`, `message`, `sent`) VALUES
-(1, 1, 3, 'Bonjour', '2013-02-25 20:17:00'),
-(2, 3, 1, 'Salut', '2013-02-25 20:18:00'),
-(3, 1, 3, 'Tu aimes les tomates ?', '2013-02-25 20:18:00'),
-(4, 3, 1, 'Oui beaucoup', '2013-02-25 20:18:00'),
-(5, 1, 3, 'Tu es mon Ã¢me soeur souhaites-tu entretenir une relation coitale rÃ©guliÃ¨re avec ma personne ?', '2013-02-25 20:18:00'),
-(6, 3, 1, 'Avec plaisir, cordialement', '2013-02-25 20:21:00');
+INSERT INTO `chats` (`id`, `user_from`, `user_to`, `message`, `sent`)
+VALUES
+	(1,1,3,'Bonjour','2013-02-25 20:17:00'),
+	(2,3,1,'Salut','2013-02-25 20:18:00'),
+	(3,1,3,'Tu aimes les tomates ?','2013-02-25 20:18:00'),
+	(4,3,1,'Oui beaucoup','2013-02-25 20:18:00'),
+	(5,1,3,'Tu es mon Ã¢me soeur souhaites-tu entretenir une relation coitale rÃ©guliÃ¨re avec ma personne ?','2013-02-25 20:18:00'),
+	(6,3,1,'Avec plaisir, cordialement','2013-02-25 20:21:00');
 
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
+UNLOCK TABLES;
 
---
--- Structure de la table `pictures`
---
 
-CREATE TABLE IF NOT EXISTS `pictures` (
+# Affichage de la table pictures
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `pictures`;
+
+CREATE TABLE `pictures` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(250) NOT NULL,
   `legende` text NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `pictures`
---
+LOCK TABLES `pictures` WRITE;
+/*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
 
-INSERT INTO `pictures` (`id`, `url`, `legende`, `user_id`) VALUES
-(1, 'https://www.facebook.com/photo.php?fbid=3275334456106&set=o.129707217137362&type=1&theater', 'G SUI RISTERION', 1);
+INSERT INTO `pictures` (`id`, `url`, `legende`, `user_id`)
+VALUES
+	(1,'https://www.facebook.com/photo.php?fbid=3275334456106&set=o.129707217137362&type=1&theater','G SUI RISTERION',1);
 
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
+UNLOCK TABLES;
 
---
--- Structure de la table `quizzes`
---
 
-CREATE TABLE IF NOT EXISTS `quizzes` (
+# Affichage de la table quizzes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `quizzes`;
+
+CREATE TABLE `quizzes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(250) NOT NULL,
   `answer` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `quizzes`
---
+LOCK TABLES `quizzes` WRITE;
+/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
 
-INSERT INTO `quizzes` (`id`, `question`, `answer`) VALUES
-(1, 'Combien font 2 + 2 ?', '4'),
-(2, 'Les jambes de la soeur de Philippe sont-elles douces ?', 'Oh que oui !');
+INSERT INTO `quizzes` (`id`, `question`, `answer`)
+VALUES
+	(1,'Combien font 2 + 2 ?','4'),
+	(2,'Les jambes de la soeur de Philippe sont-elles douces ?','Oh que oui !');
 
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
 
---
--- Structure de la table `users`
---
 
-CREATE TABLE IF NOT EXISTS `users` (
+# Affichage de la table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `sex` varchar(30) NOT NULL,
   `birthdate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `users`
---
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 
-INSERT INTO `users` (`id`, `pseudo`, `password`, `email`, `sex`, `birthdate`) VALUES
-(1, 'Thomas', 'pass', 'thomas@guilleminot.com', 'Homme', '1993-03-30 20:09:00'),
-(2, 'Clara', 'pass', 'clara@hinton.com', 'Femme', '2013-02-25 20:10:00'),
-(3, 'jeremy', 'pass', 'jeremy@cambot.com', 'Homme', '2013-02-25 20:10:00'),
-(4, 'trÃ©cy', 'pass', 'trecy@fournier.com', 'Femme', '2013-02-25 20:11:00');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `sex`, `birthdate`)
+VALUES
+	(1,'Thomas','pass','thomas@guilleminot.com','Homme','1993-03-30 20:09:00'),
+	(2,'Clara','pass','clara@hinton.com','Femme','2013-02-25 20:10:00'),
+	(3,'Jeremy','pass','jeremy@cambot.com','Homme','2013-02-25 20:10:00'),
+	(4,'Trecy','pass','trecy@fournier.com','Femme','1990-11-23 18:30:00'),
+	(5,'toto','87cdec35d3e9e2d725e73c1e2b8221c1b3ec4d71','dsjhdskjh@hdsjkdh.cvol','enorme','2013-02-26 13:08:00');
 
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
